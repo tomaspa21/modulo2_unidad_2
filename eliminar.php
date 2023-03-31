8ff1cd1d17772e5b80504250596651c7ead6671a
tomaspa21 <palaciostomas73@gmail.com>
2023-03-31 20:03:21 -0300

    modulo2_unidad_2

---
+++ eliminar.php
@@ -0,0 +1,11 @@
+<?php
+include ("conexion.php");
+
+$id_componente =$_GET ['id'];
+mysqli_query($conexion_db, "DELETE FROM componentes WHERE id=$id_componente");
+
+
+header("Location: ver.php");
+
+
+?>
